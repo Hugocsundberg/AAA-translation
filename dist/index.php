287a3b4cc18f3e856c1ad0ae91447d1512725f6f
Hugo Sundberg <55212646+Hugocsundberg@users.noreply.github.com>
2021-01-20 23:04:21 +0100

    Clean up

---
+++ dist/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+    <link rel='icon' href='./favicon/favicon.ico' type='image/x-icon' />
+    <link rel="shortcut icon" href="./favicon/android-chrome-512x512.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="./favicon/apple-touch-icon.png">
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./AAA.css">
+    <meta name="theme-color" content="#FF5700">
+</head>
+
+<body>
+    <div class="centerContainer">
+        <textarea class="aaText"></textarea>
+        <p id="placeholderAa" class="placeholderAa placeholderText">AA AAA aAa aaa a AaAa AAA Aaa a (morse code) here</p>
+        <div id="button1" class="button1 button">
+            <p>COPY</p>
+        </div>
+        <textarea class="normalText"></textarea>
+        <p id="placeholderNormal" class="placeholderNormal placeholderText">Normal text here</p>
+        <div id="button2" class="button2 button">
+            <p>COPY</p>
+        </div>
+        <div class="button3 button">
+            <p>CLEAR ALL</p>
+        </div>
+        <div id="infobutton">
+            <h2>i</h2>
+        </div>
+        <div id="infoscreen">
+            <p>V1 Supports A-Z and punctuation</p>
+        </div>
+    </div>
+    <script src="./AAA.js" async defer></script>
+</body>
+
+</html>
